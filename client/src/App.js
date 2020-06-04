8cd4a2c584559380b1ee3cdf2c258129877efd95
Aydn <aydnhooper1997@gmail.com>
2020-06-05 11:33:40 +1200

    first commit
    removed service worker and default test stuff

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* make our own header */}
+      <header className="App-header"></header>
+      <div className="appBody">{/* page components render in here */}</div>
+    </div>
+  );
+}
+
+export default App;
