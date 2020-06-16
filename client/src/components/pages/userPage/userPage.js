d789d17cb72de5efefa7696c475eedfc4e1a23dc
Jtopolegend <52726354+Jtopolegend@users.noreply.github.com>
2020-06-16 12:02:05 +1200

    Updated userPage with className for H and P tags

--- client/src/components/pages/userPage/userPage.js
+++ client/src/components/pages/userPage/userPage.js
@@ -6,11 +6,15 @@ export default class userPage extends React.Component {
     return (
       <div>
         <section>
+          <div classname = "h1"></div>
           <h1>First Name Last Name</h1>
+          <div classname = "h3"></div>
           <h3>Profession x
           designer</h3>
+          <div classname = "h2"></div>
           <h2>Featured Project</h2>
           <img className="hero" src={placeholder} alt="placeholder"/>
+          <div classname = "p"></div>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit 
         vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim 
         blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa 
@@ -29,9 +33,11 @@ export default class userPage extends React.Component {
         <img className="placeholder6" src={placeholder} alt="placeholder"/> 
         <img className="placeholder7" src={placeholder} alt="placeholder"/> 
         <img className="placeholder8" src={placeholder} alt="placeholder"/> 
-
+        
+        <div classname = "h4"></div>
         <h4>project name
         dd/mm/yyyy</h4>
+        <div classname = "p2"></div>
     <p2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae 
         nibh. consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. consectetur 
         adipiscing elit. 
