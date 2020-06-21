2900b07f8c6e48a6831838caffe1177a40d2a13d
Guest User <Guest@VS201-H-6-8.mddn.vuw.ac.nz>
2020-06-21 20:07:09 +1200

    last one

--- client/src/components/pages/ProjectUpload/ProjectUpload3.js
+++ client/src/components/pages/ProjectUpload/ProjectUpload3.js
@@ -7,21 +7,34 @@ export default class ProjectPage3 extends React.Component {
     render() {
         return(
             <div className="container">
+            <div className="section">
+              <input
+                className="project-title"
+                type="text"
+                placeholder="PROJECT TITLE"
+              />
+              <div className="name-flex">
+                <input
+                  className="designerNames"
+                  type="text"
+                  placeholder="FIRST NAME "
+                ></input>
+                <input
+                  className="designerNames"
+                  type="text"
+                  placeholder="LAST NAME "
+                ></input>
+              </div>
+              <div/>
 
                 <div className="section">
-                    <input className="project-title" type="text" placeholder="PROJECT TITLE"></input>
-                    <div className="name-flex">
-                        <input className="designerNames" type="text" placeholder="FIRST NAME "></input>
-                        <input className="designerNames" type="text" placeholder="LAST NAME "></input>
+                <div className="flexcontainer3">
+                    <img className="lrg-img" src={landscape} alt="placeholder"/>
                     </div>
                 </div>
 
                 <div className="section">
-                    <img src={landscape} alt="placeholder"/>
-                </div>
-
-                <div className="section">
-                    <div className="flexcontainer">
+                <div className="flexcontainer1">
                         <img className="pr-hero" src={landscape} alt="placeholder"/>
                         <textarea className="synopsis-input" 
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ">
@@ -30,10 +43,10 @@ export default class ProjectPage3 extends React.Component {
                 </div>
 
                 <div className="section">
-                    <div className="flexcontainer2">
-                        <img className="portrait" src={portrait} alt="placeholder"/>
-                        <img className="portrait" src={portrait} alt="placeholder"/>
-                        <img className="portrait" src={portrait} alt="placeholder"/>
+                    <div className="flexcontainer9">
+                        <img className="portrait1" src={portrait} alt="placeholder"/>
+                        <img className="portrait2" src={portrait} alt="placeholder"/>
+                        <img className="portrait3" src={portrait} alt="placeholder"/>
                     </div>
                     <textarea className="synopsis-input2" 
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh.">
@@ -41,21 +54,21 @@ export default class ProjectPage3 extends React.Component {
                 </div>
 
                 <div className="section">
-                    <div className="flexcontainer2">
+                    <div className="flexcontainer1">
                         <img className="portrait" src={portrait} alt="placeholder"/>
-                        <img className="lrg-img" src={landscape} alt="placeholder"/>
+                        <img className="pr-hero" src={landscape} alt="placeholder"/>
                     </div>
                 </div>
 
                 <div className="section">
-                    <div className="flexcontainer2">
-                        <img className="sml-img" src={landscape} alt="placeholder"/>
-                        <textarea className="synopsis-input3" 
+                    <div className="flexcontainer1">
+                        <img className="pr-hero" src={landscape} alt="placeholder"/>
+                        <textarea className="synopsis-input" 
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu. Dignissim blandit.">
                         </textarea>
                     </div>
                 </div>
-
+            </div>
             </div>
         )
     }    
