ec7baf34b2cde097c4c5cdce49050f28e3c307d9
Guest User <Guest@VS201-H-6-8.mddn.vuw.ac.nz>
2020-06-21 19:55:20 +1200

    more restructure

--- client/src/components/pages/ProjectUpload/ProjectUpload2.js
+++ client/src/components/pages/ProjectUpload/ProjectUpload2.js
@@ -6,37 +6,48 @@ export default class ProjectUpload2 extends React.Component {
     render() {
         return(
             <div className="container">
+            <div className="section">
+              <input
+                className="project-title"
+                type="text"
+                placeholder="PROJECT TITLE"
+              />
+              <div className="name-flex">
+                <input
+                  className="designerNames"
+                  type="text"
+                  placeholder="FIRST NAME "
+                ></input>
+                <input
+                  className="designerNames"
+                  type="text"
+                  placeholder="LAST NAME "
+                ></input>
+              </div>
+              <div/>
 
                 <div className="section">
-                    <input className="project-title" type="text" placeholder="PROJECT TITLE"></input>
-                    <div className="name-flex">
-                        <input className="designerNames" type="text" placeholder="FIRST NAME "></input>
-                        <input className="designerNames" type="text" placeholder="LAST NAME "></input>
-                    </div>
-                </div>
-
-                <div className="section">
-                    <div className="flexcontainer2">
-                        <textarea className="synopsis-input" 
+                    <div className="flexcontainer7">
+                        <div className="flexcontainer8">
+                            <img className="container8-image1" src={landscape} alt="placeholder"/>
+                            <img className="container8-image2" src={landscape} alt="placeholder"/>
+                        </div>
+                        <textarea className="synopsis-input5" 
                         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies.">
                         </textarea>
-                        <div className="flexcontainer3">
-                            <img className="med-img" src={landscape} alt="placeholder"/>
-                            <img className="med-img" src={landscape} alt="placeholder"/>
-                        </div>
                     </div>
                 </div>
 
-                <div className="section">
-                    <img src={landscape} alt="placeholder"/>
+                <div className="flexcontainer3">
+                    <img className="lrg-img" src={landscape} alt="placeholder"/>
                 </div>
 
                 <div className="section">
-                    <div className="flexcontainer2">
-                        <img className="pr-hero" src={landscape} alt="placeholder"/>
-                        <div className="flexcontainer3">
-                            <img className="micro-img" src={landscape} alt="placeholder"/>
-                            <img className="micro-img" src={landscape} alt="placeholder"/>
+                    <div className="flexcontainer5">
+                        <img className="container5-image" src={landscape} alt="placeholder"/>
+                        <div className="flexcontainer6">
+                            <img className="micro-img1" src={landscape} alt="placeholder"/>
+                            <img className="micro-img2" src={landscape} alt="placeholder"/>
                         </div>
                     </div>
                     <textarea className="synopsis-input4" 
@@ -46,14 +57,15 @@ export default class ProjectUpload2 extends React.Component {
 
                 <div className="section">
                     <div className="flexcontainer2">
-                        <img className="sml-img" src={landscape} alt="placeholder"/>
-                        <img className="sml-img" src={landscape} alt="placeholder"/>
+                        <img className="sml-img1" src={landscape} alt="placeholder"/>
+                        <img className="sml-img2" src={landscape} alt="placeholder"/>
                     </div>
                     <textarea className="synopsis-input4" type="text"
                     placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim pretium sit id scelerisque auctor tellus sit vitae nibh. Aenean pellentesque imperdiet morbi vitae varius sed pellentesque aliquet ultricies. Dignissim blandit ultrices vehicula convallis. Congue massa dui eu porttitor faucibus mi neque, venenatis. Congue massa dui eu.">
                     </textarea>
                 </div>
 
+            </div>
             </div>
         );
     }    
